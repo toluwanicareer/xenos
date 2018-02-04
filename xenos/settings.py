01e743ed8b5822b119a352bc54a83bacbe3e6e8c
Toluwani <abiodun.toluwanii@gmail.com>
2018-02-04 22:18:56 +0100

    'fg'

--- xenos/settings.py
+++ xenos/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+UPLOAD_TO = os.path.join(BASE_DIR, 'media/upload')
