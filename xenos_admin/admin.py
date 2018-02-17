c0794e049fe3f23162af907763c7b813c493084e
Toluwani <abiodun.toluwanii@gmail.com>
2018-02-17 11:59:59 +0100

    'gfg'

--- xenos_admin/admin.py
+++ xenos_admin/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Plan, Investment, Percentage
+from .models import Plan, Investment, Percentage, Transaction, test_model
 
 # Register your models here.
 admin.site.register(Plan)
 admin.site.register(Investment)
 admin.site.register(Percentage)
+admin.site.register(Transaction)
+admin.site.register(test_model)
