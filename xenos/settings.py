053dd7daeae6f38b672b3156a54a5fe1631aebf3
Toluwani <abiodun.toluwanii@gmail.com>
2018-03-01 06:35:25 +0100

    fg

--- xenos/settings.py
+++ xenos/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'xenos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -104,7 +104,7 @@ DATABASES = {
     }
 }
 
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
