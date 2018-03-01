170f35823ac6f71b26e26a1a9bdb12e458fd1a26
Toluwani <abiodun.toluwanii@gmail.com>
2018-03-01 06:26:34 +0100

    fg

--- main/models.py
+++ main/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class CompanyDigits(models.Model):
+	reg=models.CharField(max_length=200)
+	capi=models.CharField(max_length=200)
+	withd=models.CharField(max_length=200)
+	depo=models.CharField(max_length=200)
